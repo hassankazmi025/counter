7f6b806cb208f7de2c3b1274ccb40eb85a5dd9cd
hassankazmi025 <hassankazmi025@gmail.com>
2024-09-07 17:07:49 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(0);
+
+  // let counter = 5;
+  const addValue = () => {
+    // counter = counter + 1;
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+
+    // setCounter(counter);
+    // if (counter > 20) {
+    //   setCounter((counter = 20));
+    //   console.log(counter);
+    // }
+  };
+  const removeValue = () => {
+    counter = counter - 1;
+    setCounter(counter);
+    if (counter < 1) {
+      setCounter((counter = 0));
+      console.log(counter);
+    }
+  };
+  return (
+    <>
+      <h1>Hello</h1>
+      <h2>Counter value:{counter}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  );
+}
+
+export default App;
